c95d52fd98edab88a1d77564623b352b371b0273
= <=>
2020-01-11 20:18:37 +0100

    First commit

---
+++ prvni_zakazka.py
@@ -0,0 +1,24 @@
+# Aritmeticke operace
+
+12 + 5
+12 - 5
+
+5 * 7
+
+15 / 8
+15 // 8
+15 % 8
+
+5 ** 3
+
+# Problém s desetinnými čísly
+1.01 + 1.01
+1.01 + 1.01 + 1.01
+
+'Ahoj'
+
+# Mezera se sama nepřidá
+'Ahoj' + 'Jirko'
+'Ahoj ' + 'Jirko'
+
+'Ahoj ' * 10
