6d010f5fd5438dd9a1fc2a24e8d51e7257c3eb42
Jiří Pešík <jiri.pesik@huld.io>
2020-04-04 23:15:59 +0200

    Update 09_cykly.py

--- 09_cykly.py
+++ 09_cykly.py
@@ -1,10 +1,19 @@
 # Lze mít i seznam v seznamu
 vydaje = [
-    ['Pavel', 'toaletní papír', 100],
+    ['Pavel', 'toaletní papír', 110],
     ['Natálie', 'vajíčka', 80],
     ['Roman', 'máslo', 50]
 ]
 
-suma_celkem = 0
 for vydaj in vydaje:
-    suma_celkem += vydaj[2]
+  print(f"{vydaj[0]} koupil(a) {vydaj[1]} za {vydaj[2]} Kč")
+
+for vydaj in vydaje:
+  if vydaj[2] > 100:
+    print(f"{vydaj[0]} koupil(a) {vydaj[1]} za {vydaj[2]} Kč")
+
+sumaCelkem = 0
+for vydaj in vydaje:
+  sumaCelkem += vydaj[2]
+
+print(f"Celkove castka je {sumaCelkem} Kč")
